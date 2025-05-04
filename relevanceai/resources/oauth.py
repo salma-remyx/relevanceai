f68871409b42fabf88c42dab0e4834a15102a9ab
Alex Jenkins <alex@relevanceai.com>
2025-05-05 01:43:08 +1000

    removed pydantic and cleaned up imports for oauth

--- relevanceai/resources/oauth.py
+++ relevanceai/resources/oauth.py
@@ -1,13 +1,9 @@
 from __future__ import annotations
-
-from .._client import RelevanceAI, AsyncRelevanceAI
-from .._resource import SyncAPIResource, AsyncAPIResource
-
-from ..resources.agent import Agent, AsyncAgent
-from typing import List, Optional
-from ..types.oauth import *
 import webbrowser
 
+from .._client import RelevanceAI
+from .._resource import SyncAPIResource
+from ..types.oauth import OAuth
 
 class OAuthManager(SyncAPIResource):
 
@@ -16,14 +12,13 @@ class OAuthManager(SyncAPIResource):
     def add_google_integration(self, auto_open: bool = False) -> str:
         path = "auth/oauth/get_url"
         body = {"provider": "google", "types": ["email-read-write"], "redirect_url": ""}
-        response = self._post(path, body=body)
-        response = OAuth(**response.json())
+        response = self._post(path, body=body, cast_to=dict)
+        response = OAuth(**response)
         if auto_open:
             webbrowser.open(response.auth_url)
         return response.auth_url
 
-    def list_active_integrations(self):
+    def list_active_integrations(self) -> dict:
         path = "auth/oauth/accounts/list"
-        response = self._post(path)
-        # return response.json()
-        return ActiveIntegrations(**response.json())
+        response = self._post(path, cast_to=dict)
+        return response
